20ac054d98dd8fcd3972562648f079519d7884fb
TRAVIS BODINE <TRABODIN@uat.edu>
2019-06-05 12:59:38 -0700

    Added some base class

---
+++ TurtleGraphics/TurtleGraphics/Classes/Turtle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurtleGraphics.Classes
+{
+    class Turtle
+    {
+        int[][] _floor;
+        int[][] _program;
+
+        enum Commands { PenUp, PenDown, TurnRight, TurnLeft, MoveForward, Print, EndOfData }
+
+
+    }
+}
---
+++ TurtleGraphics/TurtleGraphics/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurtleGraphics
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+         
+            Console.ReadKey();
+
+        }
+
+    }
+}
